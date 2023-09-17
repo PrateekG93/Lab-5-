ff71fded468a680531f5c8c283554ff6434a06d9
PrateekG93 <141636515+PrateekG93@users.noreply.github.com>
2023-09-17 16:35:43 +0530

    Update Python 5 Q7.py

--- Lab 5/Python 5 Q7.py
+++ Lab 5/Python 5 Q7.py
@@ -8,8 +8,9 @@ Created on Sat Sep 16 23:56:29 2023
 
 i= 1
 n= int(input("Enter number of lines you want to print: "))
-a=n-1
+a=n
 while i<=(n):
-    print(" "*a,"* "*i)
-    i= i+1
-    a-=1
+    while a>0:
+        print(" "*a,"* "*i)
+        i= i+1
+        a-=1
